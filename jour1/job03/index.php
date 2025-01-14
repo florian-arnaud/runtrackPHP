26d708aece5b3b655f58e958161ed209b7eec3e8
florian-arnaud <florian.arnaud@laplateforme.io>
2025-01-14 09:29:30 +0100

    Added first and second day jobs

---
+++ jour1/job03/index.php
@@ -0,0 +1,38 @@
+
+<?php
+$myBoolean = true;
+$myInteger = 13;
+$myFloat = 1.3;
+$myString = "Je suis une chaîne.";
+//Je ne sais pas si vous demandez d'utiliser des fonctions natives où de mettre directement la valeur.
+echo "<table>
+<thead>
+<tr>
+    <th>Type</th>
+    <th>Nom</th>
+    <th>Valeur</th>
+<tbody>
+<tr>
+<td>Booléen</td>
+<td>myBoolean</td>
+<td>{$myBoolean}</td>
+</tr>
+
+<tr>
+<td>Entier</td>
+<td>myInteger</td>
+<td>{$myInteger}</td>
+</tr>
+
+<tr>
+<td>Nombre à virgule flottante</td>
+<td>myFloat</td>
+<td>{$myFloat}</td>
+</tr>
+
+<tr>
+<td>Chaîne de caractère</td>
+<td>myString</td>
+<td>{$myString}</td>
+</tr>";
+
---
+++ jour2/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+for ($i=0; $i < 1338; $i++) { 
+
+    if ($i != 42) {
+        echo $i;
+        echo "</br>";
+    }
+
+    else 
+
+    {
+        echo "<b><u>" . $i . "</b></u>";
+        echo "</br>";
+    }
+
+}
